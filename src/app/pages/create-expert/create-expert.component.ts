8a04e4341e00f29918fad94987a97d78c96df3a4
Sara <sbr.90@hotmail.com>
2021-04-28 08:03:07 +0200

    u

--- src/app/pages/create-expert/create-expert.component.ts
+++ src/app/pages/create-expert/create-expert.component.ts
@@ -2,70 +2,140 @@ import { taggedTemplate } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import {ExpertsService} from './../../services/experts.service';
-
+import { ExpertsService } from './../../services/experts.service';
 
 @Component({
   selector: 'app-create-expert',
   templateUrl: './create-expert.component.html',
-  styleUrls: ['./create-expert.component.scss']
+  styleUrls: ['./create-expert.component.scss'],
 })
 export class CreateExpertComponent implements OnInit {
-  formGroup: FormGroup = new FormGroup({})
-  expert=true;
-  state = [{value: 'validado'},{ value: 'por validar'}];
-  rating = [{value: '15'},{ value: '25'},{ value: '55'},{ value: '75'},{ value: '100'}];
-  availability = [{value: 'mañana'}, {value:'tarde'}];
+  formGroup: FormGroup = new FormGroup({});
+  expert = true;
+  state = [{ value: 'validado' }, { value: 'por validar' }];
+  rating = [
+    { value: '15' },
+    { value: '25' },
+    { value: '55' },
+    { value: '75' },
+    { value: '100' },
+  ];
+  availability = [{ value: 'mañana' }, { value: 'tarde' }];
+  tagsContainer=[];
 
-
-  constructor(private formBuilder: FormBuilder,private router: Router, private expertService: ExpertsService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private expertService: ExpertsService
+  ) {}
 
   ngOnInit(): void {
-
     this.formGroup = this.formBuilder.group({
-      name: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
-      surname: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
-      mail: ['', Validators.compose([Validators.required, Validators.email])],
-      linkedln: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
-      phone:  ['', Validators.compose([Validators.required, Validators.minLength(5)])], 
-      address:  ['', Validators.compose([Validators.required, Validators.minLength(5)])], 
-      dni:  ['', Validators.compose([Validators.required, Validators.minLength(5)])], 
+      name: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(10),
+        ]),
+      ],
+      surname: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(10),
+        ]),
+      ],
+      mail: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.email,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ]),
+      ],
+      linkedln: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
+      phone: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
+      address: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
+      dni: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
       tags: this.formBuilder.array([]),
-      state:  ['', Validators.compose([Validators.required, Validators.minLength(5)])], 
-      rating:  ['', Validators.compose([Validators.required, Validators.maxLength(3)])], 
-      availability:  ['', Validators.compose([Validators.required, Validators.minLength(2)])], 
+      state: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
+      rating: [
+        '',
+        Validators.compose([Validators.required, Validators.maxLength(3)]),
+      ],
+      availability: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(2)]),
+      ],
     });
-
-    console.log(this.formGroup)
   }
-  get tagsArray(): FormArray{
-    return this.formGroup.get('tags') as FormArray
+  get tagsArray(): FormArray {
+    return this.formGroup.get('tags') as FormArray;
   }
 
-  addTag() {
-
-    const tags = this.formBuilder.group(
-      {
-        name: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(2)])]
-      }
-    );
-    this.tagsArray.push(tags);
-
+  addTag() { 
+    
+    let tags = this.formBuilder.group({
+      name: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(2),
+        ]),
+      ],
+    });
+    console.log(tags)
+    this.tagsArray.push(tags)
   }
+
 
   removeTag(index: number) {
     this.tagsArray.removeAt(index);
   }
 
+  submitRegisterForm() {
 
+    if (
+      this.formGroup.value.name &&
+      this.formGroup.value.surname &&
+      this.formGroup.value.phone &&
+      this.formGroup.value.mail&&
+      this.formGroup.value.rating&&
+      this.formGroup.value.dni&&
+      this.formGroup.value.linkedln&&
+      this.tagsArray.value.length >= 1&&
+      this.formGroup.value.availability
 
-  submitRegisterForm(){
-     this.expertService.register(this.formGroup.value).subscribe((response) => {
-      if(response){
-       alert(`registro con exito`);
-       }else{
-        alert('Error: No Token Received');
-      }
-     })
+    ) {
+      this.expertService
+        .register(this.formGroup.value)
+        .subscribe((response) => {
+          if (response) {
+            alert(`registro con exito`);
+          } else {
+            alert('Error: No Token Received');
+          }
+        });
+    } else {
+      alert('por favor, rellene todos los campos');
+    }
   }
 }
